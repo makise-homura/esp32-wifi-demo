c19549dca81bf96ce84e4a65f596e06aed2f9057
Igor Molchanov <akemi_homura@kurisa.ch>
2026-03-31 21:46:16 +0300

    Add initial version of sketch

---
+++ esp32-wifi-demo.ino
@@ -0,0 +1,56 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <TimeLib.h>
+
+const char* wifi_ssid        = "133-2.4G";
+const char* wifi_password    = "f2line..";
+const char* ntp_server       = "0.ru.pool.ntp.org";
+const int   ntp_correction   = +3;
+const int   ntp_interval     = 60;
+const int   gpio_led         = 8;
+
+enum {S_INIT, S_CONN, S_TIME} status;
+
+WiFiUDP ntpUDP;
+NTPClient *timeClient;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing..");
+  pinMode(gpio_led, OUTPUT);
+  WiFi.begin(wifi_ssid, wifi_password);
+  timeClient = new NTPClient(ntpUDP, ntp_server, ntp_correction * 3600, ntp_interval * 1000); 
+  timeClient->begin();
+  status = S_INIT;
+}
+
+void loop() {
+  switch(status)
+  {
+    case S_INIT:
+      if (WiFi.status() == WL_CONNECTED)
+      {
+        Serial.printf("\r\nConnected to the WiFi network\r\nIP address: %s\r\n", WiFi.localIP().toString().c_str());
+        timeClient->update();
+        status = S_CONN;
+      }
+      break;
+    case S_CONN:
+      if (timeClient->isTimeSet())
+      {
+        setTime(timeClient->getEpochTime());
+        Serial.printf("\r\nSynchronized system time from %s\r\nCurrent time: %s\r\n", ntp_server, timeClient->getFormattedTime());
+        status = S_TIME;
+      }
+      break;
+  }
+
+  time_t t = now();
+  int half = millis() % 1000 < 500;
+  char s = half ? ':': ' ';
+  Serial.printf("%02d%c%02d%c%02d %c\r", hour(t), s, minute(t), s, second(t), status >= S_TIME ? '+' : status >= S_CONN ? '*' : '?');
+  digitalWrite(gpio_led, half ? LOW : HIGH);
+  delay(100);
+}
